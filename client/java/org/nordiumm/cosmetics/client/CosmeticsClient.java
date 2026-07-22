88496032af852745734989d88a1439fb273469b4
Nordiumm <kryptanseplar@gmail.com>
2026-07-23 00:17:44 +0200

    Upload

---
+++ client/java/org/nordiumm/cosmetics/client/CosmeticsClient.java
@@ -0,0 +1,10 @@
+package org.nordiumm.cosmetics.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CosmeticsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ main/java/org/nordiumm/cosmetics/Cosmetics.java
@@ -0,0 +1,10 @@
+package org.nordiumm.cosmetics;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cosmetics implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
